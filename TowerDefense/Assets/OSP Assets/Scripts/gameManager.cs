0e3d2fef43cc0f22a941f9631504f0b76f3670be
Kevin Malmsten <outofspacegamedevelopment@gmail.com>
2015-01-25 16:35:58 -0600

    Setting Up RayCast Placement

---
+++ TowerDefense/Assets/OSP Assets/Scripts/gameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour
+{
+	public LayerMask PlaceLayer;
+	Vector3 TargetPosition;
+	public GameObject Hero;
+
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Press");
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, PlaceLayer))
+			{
+				Instantiate(Hero, hit.point, Hero.transform.rotation);
+			}
+		}
+	}
+
+}
